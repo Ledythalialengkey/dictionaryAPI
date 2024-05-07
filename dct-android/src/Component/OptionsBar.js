dcadd828cfe449a8f6b97db6a5e711121305af5d
gilbert_lua <gilbert.lua@formulatrix.com>
2024-05-07 13:20:53 +0700

    add mobile app

---
+++ dct-android/src/Component/OptionsBar.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+export default function OptionsBar() {
+  return (
+      <>       
+        <Container>
+          <Row>
+            <Col>1 of 1</Col>
+          </Row>
+        </Container>
+      </>    
+  );
+}
---
+++ dct-android/src/Main.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import OptionsBar from './Component/OptionsBar';
+import  Container  from 'react-bootstrap/Container';
+export default function Main() {
+    return (
+      <>
+        <Container>
+          <OptionsBar/>
+        </Container>
+      </>
+    );
+  }
